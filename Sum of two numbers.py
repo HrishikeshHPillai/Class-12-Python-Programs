c10d30a1f86dac9251909cb0991a018c48f66bf3
HrishikeshHPillai <57534793+HrishikeshHPillai@users.noreply.github.com>
2019-11-10 20:47:55 +0530

    Add files via upload

---
+++ Sum of two numbers.py
@@ -0,0 +1,8 @@
+#Title:Sum of two numbers
+#Author:Hrishikesh H Pillai
+#Date:10-11-2019
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+sum=a+b
+print(a,"+",b,"=",sum)
